ffbd166a6befecd65c21b80da89a7e08e00d0679
Ahmad <23100222@lums.edu.pk>
2022-03-17 18:30:03 +0500

    Update Database
    improved database design

--- server/schema/bloodlink.sql
+++ server/schema/bloodlink.sql
@@ -16,6 +16,19 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Temporary view structure for view `active_reqs`
+--
+
+DROP TABLE IF EXISTS `active_reqs`;
+/*!50001 DROP VIEW IF EXISTS `active_reqs`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `active_reqs` AS SELECT 
+ 1 AS `req_id`,
+ 1 AS `deadline`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Table structure for table `admin`
 --
 
@@ -23,7 +36,7 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admin` (
-  `admin_id` int unsigned NOT NULL,
+  `admin_id` int unsigned NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(45) NOT NULL,
   `email` varchar(90) NOT NULL,
   `password` varchar(45) NOT NULL,
@@ -54,12 +67,11 @@ CREATE TABLE `blood_req` (
   `attendant_name` varchar(45) NOT NULL,
   `attendant_num` int NOT NULL,
   `blood_group` varchar(3) NOT NULL,
-  `date` date NOT NULL,
-  `time` time NOT NULL,
+  `deadline` datetime(3) NOT NULL,
   `hospital` varchar(135) NOT NULL,
   `quantity` varchar(45) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
-  `user_id` int DEFAULT NULL,
+  `user_contact_num` int DEFAULT NULL,
   `admin_id` int DEFAULT NULL,
   `moderator_id` int DEFAULT NULL,
   PRIMARY KEY (`req_id`),
@@ -91,7 +103,7 @@ CREATE TABLE `catalog` (
   `blood_group` varchar(3) DEFAULT NULL,
   `vaccinated` tinyint(1) DEFAULT NULL,
   `last_donated` datetime(4) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
+  `city` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -114,9 +126,9 @@ DROP TABLE IF EXISTS `donor_history`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `donor_history` (
-  `user_id` int NOT NULL,
+  `user_contact_num` int NOT NULL,
   `request_id` int NOT NULL,
-  PRIMARY KEY (`user_id`,`request_id`)
+  PRIMARY KEY (`user_contact_num`,`request_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -130,6 +142,37 @@ LOCK TABLES `donor_history` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `donors`
+--
+
+DROP TABLE IF EXISTS `donors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donors` (
+  `user_contact_num` int unsigned NOT NULL,
+  `blood_group` varchar(3) NOT NULL,
+  `diabetes` tinyint(1) DEFAULT '0',
+  `blood_disease` tinyint(1) DEFAULT '0',
+  `vaccinated` tinyint(1) DEFAULT '0',
+  `gender` varchar(20) DEFAULT NULL,
+  `last_donated` datetime(3) DEFAULT NULL,
+  `region` varchar(135) NOT NULL,
+  `plasma` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`user_contact_num`),
+  UNIQUE KEY `user_contact_num_UNIQUE` (`user_contact_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `donors`
+--
+
+LOCK TABLES `donors` WRITE;
+/*!40000 ALTER TABLE `donors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `donors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `moderator`
 --
 
@@ -137,8 +180,9 @@ DROP TABLE IF EXISTS `moderator`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `moderator` (
-  `moderator_id` int unsigned NOT NULL,
+  `moderator_id` int unsigned NOT NULL AUTO_INCREMENT,
   `moderator_name` varchar(45) NOT NULL,
+  `admin_id` int unsigned NOT NULL,
   `email` varchar(90) NOT NULL,
   `password` varchar(45) NOT NULL,
   `contact_num` int NOT NULL,
@@ -157,27 +201,35 @@ LOCK TABLES `moderator` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `notify`
+-- Table structure for table `plasma_req`
 --
 
-DROP TABLE IF EXISTS `notify`;
+DROP TABLE IF EXISTS `plasma_req`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `notify` (
-  `notify_id` int NOT NULL,
-  `request_id` int NOT NULL,
+CREATE TABLE `plasma_req` (
+  `req_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `attendant_name` varchar(45) NOT NULL,
+  `attendant_num` int NOT NULL,
+  `blood_group` varchar(3) NOT NULL,
   `deadline` datetime(3) NOT NULL,
-  PRIMARY KEY (`notify_id`)
+  `hospital` varchar(135) NOT NULL,
+  `quantity` varchar(45) NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
+  `user_id` int DEFAULT NULL,
+  `admin_id` int DEFAULT NULL,
+  `moderator_id` int DEFAULT NULL,
+  PRIMARY KEY (`req_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `notify`
+-- Dumping data for table `plasma_req`
 --
 
-LOCK TABLES `notify` WRITE;
-/*!40000 ALTER TABLE `notify` DISABLE KEYS */;
-/*!40000 ALTER TABLE `notify` ENABLE KEYS */;
+LOCK TABLES `plasma_req` WRITE;
+/*!40000 ALTER TABLE `plasma_req` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plasma_req` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -188,22 +240,14 @@ DROP TABLE IF EXISTS `registered_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `registered_user` (
-  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_contact_num` int unsigned NOT NULL,
   `user_name` varchar(45) NOT NULL,
   `email` varchar(90) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `contact_num` int unsigned NOT NULL,
-  `blood_group` varchar(3) DEFAULT NULL,
-  `diabetes` tinyint(1) DEFAULT '0',
-  `blood_disease` tinyint(1) DEFAULT '0',
-  `vaccinated` tinyint(1) DEFAULT '0',
   `gender` varchar(20) DEFAULT NULL,
   `donor` tinyint(1) DEFAULT '0',
-  `last_donated` datetime(4) DEFAULT NULL,
-  `address` varchar(135) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
-  UNIQUE KEY `contact_num_UNIQUE` (`contact_num`)
+  PRIMARY KEY (`user_contact_num`),
+  UNIQUE KEY `contact_num_UNIQUE` (`user_contact_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -215,6 +259,24 @@ LOCK TABLES `registered_user` WRITE;
 /*!40000 ALTER TABLE `registered_user` DISABLE KEYS */;
 /*!40000 ALTER TABLE `registered_user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Final view structure for view `active_reqs`
+--
+
+/*!50001 DROP VIEW IF EXISTS `active_reqs`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `active_reqs` AS select `blood_req`.`req_id` AS `req_id`,`blood_req`.`deadline` AS `deadline` from `blood_req` where (`blood_req`.`status` = 1) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -225,4 +287,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-12 16:36:05
+-- Dump completed on 2022-03-17 18:28:15
