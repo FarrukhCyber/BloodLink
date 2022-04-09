87bf46624516f921bb231509d8f0d833ec1a23bb
MuhammadHassnain <23100250@lums.edu.pk>
2022-04-09 20:34:10 +0500

    Calender and clock added

--- lib/createBloodRequest/createBloodRequest.dart
+++ lib/createBloodRequest/createBloodRequest.dart
@@ -45,7 +45,9 @@ class CreateBloodRequest extends StatelessWidget {
               inputController: TextEditingController(),
               hintText: "Required",
               labelText: "Attendant Phone Number"),
-          DropDownMenu()
+          DropDownMenu(),
+          getDate(title: "Please select a date"),
+          getTime()
         ],
       ),
     );
@@ -218,6 +220,7 @@ class _DropDownMenuState extends State<DropDownMenu> {
         margin: EdgeInsets.fromLTRB(
             0, MediaQuery.of(context).size.height * 0.03, 0, 0),
         child: DropdownButton(
+          isExpanded: true,
           value: dropDownValue,
           icon: Icon(Icons.keyboard_arrow_down),
           items: items.map((String items) {
@@ -229,5 +232,111 @@ class _DropDownMenuState extends State<DropDownMenu> {
             });
           },
         ));
+  }
+}
+
+class getDate extends StatefulWidget {
+  getDate({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DropDownState createState() => _DropDownState();
+}
+
+class _DropDownState extends State<getDate> {
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+      builder: (context, child) => Theme(
+        data: ThemeData().copyWith(
+          colorScheme: ColorScheme.dark(
+            primary: Color.fromARGB(255, 222, 44, 44),
+            onPrimary: Colors.white,
+            surface: Color.fromARGB(255, 222, 44, 44),
+            onSurface: Colors.black,
+          ),
+          dialogBackgroundColor: Colors.white,
+        ),
+        child: child!,
+      ),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      margin: EdgeInsets.fromLTRB(
+          0, MediaQuery.of(context).size.height * 0.03, 0, 0),
+      child: Column(
+        children: <Widget>[
+          Text("${selectedDate.toLocal()}".split(' ')[0]),
+          SizedBox(
+            height: 0,
+          ),
+          RaisedButton(
+            onPressed: () => _selectDate(context),
+            child: Text('Please select date on which blood is required'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class getTime extends StatefulWidget {
+  @override
+  _getTimeState createState() {
+    return _getTimeState();
+  }
+}
+
+class _getTimeState extends State<getTime> {
+  TimeOfDay _selectedTime = TimeOfDay.now();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      margin: EdgeInsets.fromLTRB(
+          0, MediaQuery.of(context).size.height * 0.03, 0, 0),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                _selectTime(context);
+              },
+              child: Text("Please select time by which blood is requried"),
+            ),
+            Text("${_selectedTime.hour}:${_selectedTime.minute}"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _selectTime(BuildContext context) async {
+    final TimeOfDay? obtainedTime = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+      initialEntryMode: TimePickerEntryMode.dial,
+    );
+    if (obtainedTime != null && obtainedTime != _selectedTime) {
+      setState(() {
+        _selectedTime = obtainedTime;
+      });
+    }
   }
 }
