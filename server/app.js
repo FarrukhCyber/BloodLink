7415b823ca8615ccb4f7a99c60fb837c9a7f654a
Daim Armaghan <23100221@lums.edu.pk>
2022-04-12 13:33:19 +0500

    successful

--- server/app.js
+++ server/app.js
@@ -15,8 +15,8 @@ app.listen(port, () => console.log("Listening on Port:", port))
 //DB Connection-----------
 dbConnect = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/myapp",
-        //await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
+        //await mongoose.connect("mongodb://localhost:27017/myapp",
+        await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
         {useNewUrlParser:true, useUnifiedTopology:true})
         console.log("DB connected")
     }
