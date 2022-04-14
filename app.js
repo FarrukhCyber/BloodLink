e25d0115be8b402202b5d494fe4ed8b9e6725dc2
Farrukh <muhammadfarrukh2001@gmail.com>
2022-04-14 12:39:36 +0500

    trying

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const express = require('express')
-const port = 8080 || process.env.Port
+const port = process.env.Port || 8080  
 const app = express()
 //const submitRequestRoutes = require('./src/routes/submitRequestRoutes')
 const registerAsDonor = require('./src/routes/registerAsDonor')
@@ -13,19 +13,19 @@ app.use(express.json({extended:false}))
 app.listen(port, () => console.log("Listening on Port:", port))
 
 //DB Connection-----------
-dbConnect = async () => {
-    try {
-        //await mongoose.connect("mongodb://localhost:27017/myapp",
-        await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
-        {useNewUrlParser:true, useUnifiedTopology:true})
-        console.log("DB connected")
-    }
-    catch(err) {
-        console.log(`${err} Ocurred while connecting to DB`)
-    }
-}
+// dbConnect = async () => {
+//     try {
+//         //await mongoose.connect("mongodb://localhost:27017/myapp",
+//         await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
+//         {useNewUrlParser:true, useUnifiedTopology:true})
+//         console.log("DB connected")
+//     }
+//     catch(err) {
+//         console.log(`${err} Ocurred while connecting to DB`)
+//     }
+// }
 
-dbConnect()
+// dbConnect()
 //-------------------------
 
 
