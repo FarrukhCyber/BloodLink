0a0f1cfca8dcec21d9dca195fdb328621e08fbb8
daimakram <85197435+daimakram@users.noreply.github.com>
2022-04-15 06:26:33 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const myRequests = require('./src/routes/myRequests')
 const User = require('./src/models/user_model')
 const auth = require('./src/routes/auth.js')
 const checkDb = require('./src/routes/checkDb')
-
+const status = require('./src/routes/statusChange')
+const donorAuth = require('./src/routes/donorAuth.js')
 app.use(express.json({extended:false}))
 
 app.listen(port, () => console.log("Listening on Port:", port))
@@ -41,6 +42,8 @@ app.use("/checkDb", checkDb)
 app.use('/auth', auth)
 app.use("/register_donor", registerAsDonor)
 app.use("/my_requests", myRequests)
+app.use('/status',status)
+app.use("/donor_auth",donorAuth)
 // 404 page
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
