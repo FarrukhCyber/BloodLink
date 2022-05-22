9387e78a4e881c05880046b4854f347265a607ca
ShaddAhmed14 <23100214@lums.edu.pk>
2022-05-22 13:21:19 +0300

    updating app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const express = require('express')
 const port = 8080 || process.env.Port
 const app = express()
+
 const submitRequestRoutes = require('./src/routes/submitRequestRoutes')
 const registerAsDonor = require('./src/routes/registerAsDonor')
 const myRequests = require('./src/routes/myRequests')
@@ -21,8 +22,8 @@ app.listen(port, () => console.log("Listening on Port:", port))
 //DB Connection-----------
 dbConnect = async () => {
     try {
-        //await mongoose.connect("mongodb://localhost:27017/myapp",
-        await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
+        await mongoose.connect("mongodb://localhost:27017/course_info",
+        // await mongoose.connect("mongodb+srv://dbAdmin:9GRY24WWlW6lRwjS@cluster0.t0zsi.mongodb.net/BloodLink?retryWrites=true&w=majority",
         {useNewUrlParser:true, useUnifiedTopology:true})
         console.log("DB connected")
     }
@@ -37,6 +38,10 @@ dbConnect()
 
 app.get('/', (req, res) => {
     res.send("Hello World")
+})
+
+app.get('/random', (req, res) => {
+    res.send("Sup")
 })
 
 app.use('/auth', auth)
