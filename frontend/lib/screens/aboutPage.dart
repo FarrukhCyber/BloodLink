2ee12c8d1ac67a1a19bee3893b90e0cc1f2b922b
ShaddAhmed14 <23100214@lums.edu.pk>
2022-06-09 21:18:07 +0300

    added about

---
+++ frontend/lib/screens/aboutPage.dart
@@ -0,0 +1,7 @@
+import 'package:bloodlink/base_url.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:bloodlink/screens/homepage.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
