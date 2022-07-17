da83a93729c6a86203f097a9db426db5d65be0fc
daimakram <85197435+daimakram@users.noreply.github.com>
2022-07-17 11:35:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const donorCatalouge = require("./src/routes/donorCat")
 const rejectEmail = require("./src/routes/rejectEmail")
 const pendingRequest = require("./src/routes/pendingRequests")
 const sendEmail = require("./src/routes/sendEmail")
+const editProfile = require('./src/routes/editProfile')
 
 app.use(express.json({extended:false}))
 
@@ -62,3 +63,9 @@ app.use("/cat", donorCatalouge);
 app.use("/rejectEmail" , rejectEmail);
 app.use("/pending_request", pendingRequest);
 app.use("/sendEmail" , sendEmail);
+app.use("/editProfile", editProfile)
+
+// 404 page
+// app.use((req, res) => {
+//     res.status(404).render('404', { title: '404' });
+//   });
